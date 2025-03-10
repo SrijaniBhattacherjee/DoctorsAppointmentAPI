71ac49917b347920cd0f9fe5be705efd50f48dca
SrijaniBhattacherjee <srijanibhattacharjee123@gmail.com>
2025-03-10 23:31:26 +0530

    Added comment for context class

--- DoctorsAppointmentAPI/Models/AppointmentDbContext.cs
+++ DoctorsAppointmentAPI/Models/AppointmentDbContext.cs
@@ -4,6 +4,7 @@ namespace DoctorsAppointmentAPI.Models
 {
     public class AppointmentDbContext : DbContext
     {
+        //Table model configurations injected for app
         public AppointmentDbContext(DbContextOptions<AppointmentDbContext> options) : base(options){ }
 
         public DbSet <Patient> Patients { get; set; }
